8fe073727e179e13ebd2d096bf52d8d547be3025
Ethanb1 <Ethanballard000@gmail.com>
2015-08-23 15:41:33 +0100

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1321,7 +1321,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["-----EKB1 (XBOX 1 GT)-----"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
