8e01e04823a945e282959aae29e11e9d767c4902
Ethanb1 <Ethanballard000@gmail.com>
2015-08-23 15:37:14 +0100

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -2,7 +2,7 @@
 
 Copyright (c) 2015 Apostolique
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
+Permission is hereby granted, free of charge, to any person obtaining a COPY
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -1321,7 +1321,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["(EKB1) I/8PYlb9S"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
